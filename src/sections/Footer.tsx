4e67823d2daccd097d59abcb195b31b7c6ef95b0
Mahesh Moholkar <maheshmoholkar171@gmail.com>
2025-07-03 16:42:26 +0530

    add footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,34 @@
+import Image from "next/image";
+import logo from "@/assets/logosaas.png";
+import SocialInstagram from "@/assets/social-insta.svg";
+import SocialLinkedin from "@/assets/social-linkedin.svg";
+import SocialPinterest from "@/assets/social-pin.svg";
+import SocialYoutube from "@/assets/social-youtube.svg";
+import SocialX from "@/assets/social-x.svg";
+
 export const Footer = () => {
-  return null;
+  return (
+    <footer className="bg-black text-[#BCBCBC] text-sm py-10 text-center">
+      <div className="container">
+        <div className="inline-flex relative before:content-[''] before:top-1 before:bottom-0 before:blur before:absolute before:w-full before:h-full before:bg-[linear-gradient(to_right,#F87BFF,#FB92CF,#FFDD9B,#C2F081,#F2D8FE)]">
+          <Image src={logo} alt="Logo" height={40} className="relative" />
+        </div>
+        <nav className="flex flex-col md:flex-row md:justify-center gap-6 mt-6">
+          <a href="#">Home</a>
+          <a href="#">Features</a>
+          <a href="#">Customers</a>
+          <a href="#">Pricing</a>
+          <a href="#">Careers</a>
+        </nav>
+        <div className="flex justify-center gap-6 mt-6">
+          <SocialInstagram />
+          <SocialLinkedin />
+          <SocialPinterest />
+          <SocialYoutube />
+          <SocialX />
+        </div>
+        <p className="mt-6">&copy; 2025 Saas. All rights reserved.</p>
+      </div>
+    </footer>
+  );
 };
